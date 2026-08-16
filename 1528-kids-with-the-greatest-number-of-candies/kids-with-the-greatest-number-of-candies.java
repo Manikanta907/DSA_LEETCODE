0de3dedf2be85928967ecca0517f96309cae8def
Tanukula Neela Prasanna Manikanta <tanukulaneelaprasannamanikanta@gmail.com>
2026-08-16 22:08:53 +0530

    Time: 1 ms (96.31%) | Memory: 43.6 MB (90.70%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+        int max = candies[0];
+        List<Boolean> result = new ArrayList<>();
+
+        for(int i=1;i<candies.length;i++){
+            if(max<candies[i]){
+                max = candies[i];
+            }
+
+        }
+        for(int i=0;i<candies.length;i++){
+            if(candies[i]+extraCandies >= max){
+                result.add(true); 
+            }
+            else{
+                result.add(false);
+            }
+        }
+        return result;
+
+        
+    }
+}
