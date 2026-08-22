e5cc93d102f095c777dd587b262d8b200cfe48e7
Tanukula Neela Prasanna Manikanta <tanukulaneelaprasannamanikanta@gmail.com>
2026-08-22 21:16:36 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (89.61%) - LeetSync

---
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public Node connect(Node root) {
+
+        Node curr = root;
+
+        while (curr != null) {
+
+            Node dummy = new Node(0);
+            Node tail = dummy;
+
+            // Traverse current level using next pointers
+            while (curr != null) {
+
+                if (curr.left != null) {
+                    tail.next = curr.left;
+                    tail = tail.next;
+                }
+
+                if (curr.right != null) {
+                    tail.next = curr.right;
+                    tail = tail.next;
+                }
+
+                curr = curr.next;
+            }
+
+            // Move to the first node of next level
+            curr = dummy.next;
+        }
+
+        return root;
+    }
+}
