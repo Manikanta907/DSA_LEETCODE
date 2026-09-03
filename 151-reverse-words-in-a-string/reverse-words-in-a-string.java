b1600325acd05634d7e7721839339ea9a9d49e4f
Tanukula Neela Prasanna Manikanta <tanukulaneelaprasannamanikanta@gmail.com>
2026-09-03 22:28:55 +0530

    Time: 6 ms (80.84%) | Memory: 44.4 MB (57.48%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,12 +1,16 @@
 class Solution {
     public String reverseWords(String s) {
         String[] str = s.trim().split("\\s+");
-        String result = "";
+        StringBuilder result = new StringBuilder();
 
         for (int i = str.length - 1; i >= 0; i--) {
-            result = result + str[i] + " ";
+            result.append(str[i]);
+
+            if (i != 0) {
+                result.append(" ");
+            }
         }
 
-        return result.trim();
+        return result.toString();
     }
 }
